e86a5824770ca1301a803213c15c3ace14e7fce7
Gerald Leenerts <gerald.leenerts@gmail.com>
2011-02-20 15:24:58 -0600

    cleared out a few more files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Vivid::Application.config.secret_token = '4e6233d33ab6d17fbefa5dfbea459a0703417cb545038bc154337039bd6f099a989876ee192699980d3123249f5e4476f25a5e040b73d7fff5453efde22b2567'
