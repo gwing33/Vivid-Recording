5c6c70a313b6edf8b69318084f5736d98c8b7527
Gerald Leenerts <gerald.leenerts@gmail.com>
2011-03-12 19:05:38 -0600

    added the css/js/img/flash folders to the root directory

--- application/views/layout.php
+++ application/views/layout.php
@@ -4,6 +4,7 @@
     <title>Vivid Recording Studio</title>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
   	<script>!window.jQuery && document.write(unescape('%3Cscript src="/javascripts/jquery.js"%3E%3C/script%3E'))</script>
+  	<link rel="stylesheet" href="/css/screen.css" type="text/css" />
   </head>
 
   <body>
